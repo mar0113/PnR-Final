5d7dc99ebadf688d0d93b48c78736d83d75db2ea
mar0113 <salvinom@gilmour.org>
2017-09-29 14:27:57 -0400

    dance test

--- student.py
+++ student.py
@@ -58,16 +58,33 @@ class Piggy(pigo.Pigo):
         print("\n---- LET'S DANCE ----\n")
         ##### WRITE YOUR FIRST PROJECT HERE
         self.to_the_right()
-        #self.to_the_left()
-        #self.now_kick()
-        #self.cha_cha()
-        #self.walk_it_by_yourself()
+        self.to_the_left()
+        self.now_kick()
+        self.walk_it_by_yourself()
 
     def to_the_right(self):
         """subroutine of dance method"""
-        for x in range(3):
+        for x in range(4):
             self.encR(10)
             self.encF(5)
+
+    def to_the_left(self):
+        """subroutine of dance method"""
+        for x in range(4)
+            self.encL(10)
+            self.encF(5)
+
+    def now_kick(self):
+        """subroutine of dance method"""
+        """loops in a 120 degree arc and moves servo"""
+        for ang in range(20, 160, 2)
+            servo(ang)
+            time.sleep(.2)
+
+    def walk_it_by_yourself(self):
+        for x in range(2)
+            self.encF(5)
+            self.encB(5)
 
     def nav(self):
         """auto pilots and attempts to maintain original heading"""
