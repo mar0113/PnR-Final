13777516d1a4d5bfafac8a3160fea1ff75742a01
mar0113 <salvinom@gilmour.org>
2017-11-10 14:25:56 -0500

    added smooth turn and datetime

--- student.py
+++ student.py
@@ -1,5 +1,6 @@
 import pigo
 import time  # import just in case students need
+import datetime
 import random
 
 # setup logs
@@ -16,6 +17,7 @@ class Piggy(pigo.Pigo):
     def __init__(self):
         """The robot's constructor: sets variables and runs menu loop"""
         print("I have been instantiated!")
+        self.start_time = datetime.datetime.utcnow()
         # Our servo turns the sensor. What angle of the servo( ) method sets it straight?
         self.MIDPOINT = 90
         # YOU DECIDE: How close can an object get (cm) before we have to stop?
@@ -162,6 +164,9 @@ class Piggy(pigo.Pigo):
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("-------- [ Press CTRL + C to stop me ] --------\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
+        right_now = datetime.datetime.utcnow()
+        difference = (right_now - self.start_time).seconds
+        print ("It took you %d seconds to run this" % difference)
         while True:
             if self.is_clear(): # no obstacles are detected by the robot
                 print("I am going to move forward!")
@@ -176,6 +181,19 @@ class Piggy(pigo.Pigo):
                     self.cruise() # robot moves forward in clear direction
                 self.encB(8) # backs up robot if it still cannot find clear path and retests right and left
             self.restore_heading() # reorients robot to original heading
+
+    def smooth_turn(self):
+        self.right_rot()
+        start = datetime.datetime.utcnow()
+        self.servo(self.MIDPOINT)
+        while True:
+            if self.dist() > 100:
+                self.stop()
+                print("I think I've found a safe place to go")
+            elif datetime.datetime.utcnow() - start > datetime.timedelta(seconds=10):
+                self.stop()
+                print("I give up")
+            time.sleep(.2)
 
     def cruise(self):
         """drive straight while path is clear"""
