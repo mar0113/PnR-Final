027f3b066e9033676e670b10c513a85f9645c1b3
mar0113 <salvinom@gilmour.org>
2017-11-29 14:24:40 -0500

    fixed left turn

--- student.py
+++ student.py
@@ -176,13 +176,10 @@ class Piggy(pigo.Pigo):
                 ### ADD DOUBLE CHECK
                 self.encB(6)  # backs up robot to try to find clear path
                 self.encR(6) # turns right to find clear path
-                if self.is_clear(): # clear path found to the right
-                    self.cruise() # robot moves forward in clear direction
-                else:  # obstacle is detected by the robot
-                    self.encL(6) # turns left to find clear path if no clear path to the right
-                        if self.is_clear(): # clear path found to the left
-                        self.cruise() # robot moves forward in clear direction
-                self.restore_heading() # reorients robot to original heading
+                self.encL(6) # turns left to find clear path
+            if self.is_clear(): # clear path found to the right
+                self.cruise() # robot moves forward in clear direction
+            self.restore_heading() # reorients robot to original heading
 
     def smooth_turn(self):
         self.right_rot()
